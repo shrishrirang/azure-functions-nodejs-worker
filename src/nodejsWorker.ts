75c580d8aa16b268bf2d02daf74aaaa624895135
Shrirang Shirodkar <shrirs@microsoft.com>
2017-11-27 11:06:45 -0800

    Hack for docker / windows

--- src/nodejsWorker.ts
+++ src/nodejsWorker.ts
@@ -1,3 +1,29 @@
+// *HACK BEGIN*
+// Temporary workaround for https://github.com/nodejs/node/issues/8897
+//
+// In standalone mode, c:\home is a junction which maps to a directory on the host (docker run -v c:\home:c:\home <container name>)
+// This hack filters all requests to fs.realpathSync and special cases the result for c:\home 
+
+var scriptMode = process.env['AzureWebJobsScriptMode'];
+
+if (scriptMode !== undefined && scriptMode.toLowerCase() === 'standalone') {
+    var fs = require('fs');
+
+    var originalFunc = fs.realpathSync;
+
+    fs.realpathSync = function () {
+        var path = arguments[0];
+        // For now, just hardcode c:\home
+        if (path !== undefined && (path.toLowerCase().indexOf('c:\\home') === 0 || path.toLowerCase().indexOf('c:\\home\\') === 0)) {
+            return path;
+        }
+
+        return originalFunc.apply(this, arguments);
+    }
+}
+
+// *HACK END*
+
 var worker;
 try {
     worker = require("../../worker-bundle.js");
